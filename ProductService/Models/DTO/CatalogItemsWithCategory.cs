ca03d3508bd6cb8f6b81f8ce8a22065ec0633e10
Angelo Bruggemans <I412999@fhict.nl>
2021-05-16 17:01:49 +0200

    Added catalog functions

---
+++ ProductService/Models/DTO/CatalogItemsWithCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductService.Models.DTO
+{
+    public class CatalogItemsWithCategory
+    {
+        public List<CatalogItem> catalogItems { get; set; }
+        public string categoryName { get; set; }
+    }
+}
